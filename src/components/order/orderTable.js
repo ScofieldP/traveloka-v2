31e2a0c1232e54b37a41f0bf71cc217a1116d6fc
ScofieldP <thienphuc.luongtran@gmail.com>
2022-04-24 16:26:07 +0700

    Layout

--- src/components/order/orderTable.js
+++ src/components/order/orderTable.js
@@ -9,6 +9,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
+import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import Modal from 'react-modal';
 import IncrementButton from '.././quantity/IncrementButton';
 import DecrementButton from '.././quantity/DecrementButton';
@@ -87,20 +88,24 @@ const OrderTable = () => {
     .map((order) =>{
         return(
         <>
-        <div className="row mt-3 border border-dark">
-                            <div className="col-3">
-                                <img src={order.img} alt="" style={{width:'150px', height:'150px'}}/>
+        <div className="row mt-3 border border-dark rounded-3">
+                            <div className="col-3 p-0">
+                                <img src={order.img} alt="" style={{width:'180px', height:'180px'}}/>
                             </div>
                             <div className = "col-7">
-                                <p className ="">
+                                <div className="food_detail">
+                                <p className ="fw-bold fs-4">
                                     {order.title}
                                 </p>
-                                <p className ="">
+                                <p className ="mb-0">
                                     {order.description}
                                 </p>
-                                <p className ="">
+                                <p className ="m-0">
                                     {order.price}
                                 </p>
+                                <p><FontAwesomeIcon icon={faShoppingCart}  /> 100+</p>
+                                </div>
+                                
                             </div>
                             <div className="col-2 ">
                                 <FontAwesomeIcon icon={faCirclePlus} className="add_btn" onClick={openModal}/>
@@ -119,15 +124,17 @@ const OrderTable = () => {
                                                         <img src={order.img} alt="" style={{width:'150px', height:'150px'}}/>
                                                     </div>
                                                     <div className = "col-7 ms-5 mt-3">
-                                                        <p className ="">
-                                                            {order.title}
-                                                        </p>
-                                                        <p className ="">
-                                                            {order.description}
-                                                        </p>
-                                                        <p className ="">
-                                                            {order.price}
-                                                        </p>
+                                                    <p className ="fw-bold fs-4">
+                                                        {order.title}
+                                                    </p>
+                                                    <p className ="mb-0">
+                                                        {order.description}
+                                                    </p>
+                                                    <p className ="m-0">
+                                                        {order.price}
+                                                    </p>
+                                                    <p><FontAwesomeIcon icon={faShoppingCart}  /> 100+</p>
+
                                                     </div>
                                                 </div>
                                                 
@@ -146,7 +153,7 @@ const OrderTable = () => {
                                     <button type="button" className="btn btn-primary w-100 mt-2">Thêm vào giỏ hàng</button>
                                     </Modal>
                             </div>
-                        </div>
+        </div>
 
         </>   
         )
@@ -187,26 +194,71 @@ const [drinks, setDrinks] = useState([
     const displayDrink = drinks
     .map((drink) =>{
         return(
-         <>
-          <div className="row mt-3 border border-dark">
-                            <div className="col-3">         
-                                <img src={drink.img} alt="" style={{width:'150px', height:'150px'}}/>
+        <>
+        <div className="row mt-3 border border-dark rounded-3">
+                            <div className="col-3 p-0">
+                                <img src={drink.img} alt="" style={{width:'180px', height:'180px'}}/>
                             </div>
                             <div className = "col-7">
-                                <p className ="">
+                                <div className="food_detail">
+                                <p className ="fw-bold fs-4">
                                     {drink.title}
                                 </p>
-                                <p className ="">
+                                <p className ="mb-0">
                                     {drink.description}
                                 </p>
-                                <p className ="">
+                                <p className ="m-0">
                                     {drink.price}
                                 </p>
+                                <p><FontAwesomeIcon icon={faShoppingCart}  /> 100+</p>
+                                </div>
+                                
                             </div>
-                            <div className="col-2">
-                                <FontAwesomeIcon icon={faCirclePlus} className="add_btn" />
+                            <div className="col-2 ">
+                                <FontAwesomeIcon icon={faCirclePlus} className="add_btn" onClick={openModal}/>
+                                    <Modal
+                                    isOpen={modalIsOpen}
+                                    onRequestClose={closeModal}
+                                    style={customStyles}
+                                    contentLabel="Example Modal">
+                                    <button onClick={closeModal}>close</button>
+                                    <div className="container">
+                                    <div className="row border border-dark">
+                                        {/* Thêm vào giỏ hàng */}
+                                        <div className="col-9 ">
+                                                <div className="row px-4 py-4">
+                                                    <div className="col-3">
+                                                        <img src={drink.img} alt="" style={{width:'150px', height:'150px'}}/>
+                                                    </div>
+                                                    <div className = "col-7 ms-5 mt-3">
+                                                        <p className ="fs-4 fw-bold">
+                                                            {drink.title}
+                                                        </p>
+                                                        <p className ="mb-0">
+                                                            {drink.description}
+                                                        </p>
+                                                        <p className ="m-0">
+                                                            {drink.price}
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                            <div className="col-3 ">
+                                                <div className="btns">
+                                                <DecrementButton onClickFunc={decrementCounter}/> 
+                                                <Display message={counter}/> 
+                                                <IncrementButton onClickFunc={incrementCounter}/>  
+                                                </div>
+                                                
+                                            </div>
+                                            </div>
+                                    </div>
+                                    {/* Nút thêm */}
+                                    <button type="button" className="btn btn-primary w-100 mt-2">Thêm vào giỏ hàng</button>
+                                    </Modal>
                             </div>
-                        </div>
+        </div>
 
          </>   
         )
@@ -246,8 +298,8 @@ const [desserts, setDesserts] = useState([
     const displayDesserts = desserts
     .map((dessert) =>{
         return(
-         <>
-          <div className="row mt-3 border border-dark">
+        <>
+        <div className="row mt-3 border border-dark">
                             <div className="col-3">         
                                 <img src={dessert.img} alt="" style={{width:'150px', height:'150px'}}/>
                             </div>
@@ -391,7 +443,7 @@ const [desserts, setDesserts] = useState([
                         </div>
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
-                        <div className="container border border-dark">
+                        <div className="container border border-dark border-top-0 border-end-0">
                         <div className="order_title border-0 border-bottom border-dark"> 
                             <h4 className="text-center">Đặt món trước</h4>
                             
--- src/components/quantity/IncrementButton.js
+++ src/components/quantity/IncrementButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function IncrementButton(props) {
     return (
-        <button style={{ marginLeft: '.5rem'}} onClick={props.onClickFunc}>
+        <button style={{ marginLeft: '.5rem', borderRadius:'50%', width:'30px',height:'30px',border:'0'}} onClick={props.onClickFunc}>
         +
         </button>
       )
