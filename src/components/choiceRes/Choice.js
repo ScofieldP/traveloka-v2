f33646d8a44587a1a715bd53cb57ea1c490580d4
ScofieldP <thienphuc.luongtran@gmail.com>
2022-06-06 22:45:36 +0700

    SearchBar

--- src/components/choiceRes/Choice.js
+++ src/components/choiceRes/Choice.js
@@ -5,6 +5,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import ReactPaginate from "react-paginate";
 import Header from "../Header";
 import Footer from "../Footer";
+import SearchBar from "../SearchBar";
 import Place from "./Place";
 
 export default function Choice(props) {
@@ -29,48 +30,7 @@ export default function Choice(props) {
     <>
       <Header />
       <section className="main_container">
-        <div className="search_res">
-          <div className="container w-75 shadow-lg px-4 mt-4 ">
-            <div className="row">
-              <div className="col-4 mt-3">
-                <p>Location</p>
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="basic-addon1">
-                    <FontAwesomeIcon icon={faLocationDot} />
-                  </span>
-                  <input
-                    type="text"
-                    class="form-control shadow-none border-dark"
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                  />
-                </div>
-              </div>
-              <div className="col-8 mt-3">
-                <p>Cuisine, dish, or Restaurant name</p>
-                <div class="input-group mb-3">
-                  <span class="input-group-text" id="basic-addon1">
-                    <FontAwesomeIcon icon={faSearch} />
-                  </span>
-                  <input
-                    type="text"
-                    class="form-control shadow-none border-dark"
-                    placeholder="Username"
-                    aria-label="Username"
-                    aria-describedby="basic-addon1"
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="btn_search  d-flex justify-content-end">
-              <button type="button" class="btn btn-warning btn-md my-1 ">
-                <FontAwesomeIcon icon={faSearch} className="me-2" />
-                Search
-              </button>
-            </div>
-          </div>
-        </div>
+       <SearchBar/>
         <div className="bread_crum">
           <div className="container w-75 ">
             <p>Eats/ A Place/ Asian</p>
--- src/components/restaurant/Restaurant.js
+++ src/components/restaurant/Restaurant.js
@@ -1,57 +1,10 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import Axios from "axios";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
+import SearchBar from "../SearchBar"
 import Footer from "../Footer";
 import Header from "../Header";
-import { CONNECTION_STRING } from "../../config/index";
 const Restaurant = () => {
-  const [res, setRes] = useState("");
-  const [food, setFood] = useState("");
-  const [data, setdata] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
-  const navigate = useNavigate();
 
-  async function Search(e) {
-    e.preventDefault();
-
-    const resData = {
-      keyfind: res ? res : "",
-    };
-    const foodData = {
-      keyfind: food ? food : "",
-    };
-    if (res !== "") {
-      try {
-        const res = await Axios.post(
-          CONNECTION_STRING + "/search/restaurant",
-          resData
-        );
-        localStorage.setItem("res", JSON.stringify(res.data.rows));
-      } catch (err) {
-        if (err.response && err.response.data.errorMessage)
-          setErrorMessage(err.response.data.errorMessage);
-        return;
-      }
-    } else {
-      try {
-        console.log(foodData);
-        const food = await Axios.post(
-          CONNECTION_STRING + `/search/food`,
-          foodData
-        );
-        localStorage.setItem("food", JSON.stringify(food.data.rows));
-      } catch (err) {
-        if (err.response && err.response.data.errorMessage)
-          setErrorMessage(err.response.data.errorMessage);
-        return;
-      }
-    }
-    navigate("/choicerestaurant");
-  }
   return (
     <>
       <Header />
@@ -59,61 +12,7 @@ const Restaurant = () => {
         <div className="back_img ">
           <img src="/assets/img/background.png" alt="logo" />
         </div>
-        <div className="search_bar">
-          <div className="container w-75 shadow-lg px-4 bg-white">
-            <form onSubmit={Search}>
-              <div className="row mt-3 py-4">
-                <p className="fs-1">
-                  Khám phá nền ẩm thực đa dạng và phong phú
-                </p>
-                <div className="col-4 mt-1">
-                  <p>Món ăn</p>
-                  <div className="input-group mb-3">
-                    <label
-                      className="input-group-text"
-                      id="basic-addon1"
-                      htmlFor="food"
-                    >
-                      <FontAwesomeIcon icon={faLocationDot} />
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control shadow-none border-dark"
-                      placeholder="Nhập món ăn "
-                      onChange={(e) => setFood(e.target.value)}
-                      id="food"
-                    />
-                  </div>
-                </div>
-                <div className="col-8 mt-1">
-                  <p>Địa điểm, tên nhà hàng</p>
-                  <div className="input-group mb-3">
-                    <label
-                      className="input-group-text"
-                      id="basic-addon1"
-                      htmlFor="name"
-                    >
-                      <FontAwesomeIcon icon={faSearch} />
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control shadow-none border-dark"
-                      placeholder="Nhập địa điểm, nhà hàng bạn mong muốn tìm"
-                      onChange={(e) => setRes(e.target.value)}
-                      id="name"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="btn_search  d-flex justify-content-end">
-                <button type="submit" className="btn btn-warning btn-md my-4 ">
-                  <FontAwesomeIcon icon={faSearch} className="me-2" />
-                  Tìm kiếm
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <SearchBar />
         <div className="about_area">
           <div
             className="container "
